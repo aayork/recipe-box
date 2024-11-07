e12082f5ce5d32745496fd44c36d2264110c6a01
Aidan York <113198662+aayork@users.noreply.github.com>
2024-11-07 15:48:15 -0500

    endpoint work

--- src/app/api/items/[id]/route.ts
+++ src/app/api/items/[id]/route.ts
@@ -8,8 +8,41 @@ interface RouteParams {
   params: { id: string };
 }
 
-export async function GET(request: NextRequest, { params }: RouteParams) {}
+export async function GET(request: NextRequest, { params }: RouteParams) {
+  const { id } = params;
+  await connectMongoDB();
+  const item = await Item.findOne({ _id: id });
+  return NextResponse.json({ item }, { status: 200 });
+}
 
-export async function POST(request: NextRequest, { params }: RouteParams) {}
+export async function PUT(request: NextRequest, { params }: RouteParams) {
+  const { id } = params;
+  const {
+    title: title,
+    description: description,
+    image: image,
+  } = await request.json();
+  await connectMongoDB();
+  await Item.findByIdAndUpdate(id, { title, description, image });
+  return NextResponse.json({ message: "item updated" }, { status: 200 });
+}
 
-export async function DELETE(request: NextRequest, { params }: RouteParams) {}
+export async function DELETE(request: NextRequest, { params }: RouteParams) {
+  const { id } = params; // Extracts the item ID from the request parameters.
+
+  // Checks if the ID is a valid MongoDB ObjectId.
+  if (!mongoose.Types.ObjectId.isValid(id)) {
+    return NextResponse.json({ message: "Invalid ID format" }, { status: 400 });
+  }
+
+  await connectMongoDB(); // Connects to the MongoDB database.
+  const deletedItem = await Item.findByIdAndDelete(id); // Attempts to delete the item with the specified ID.
+
+  // If no item was found with the provided ID, returns a 404 response.
+  if (!deletedItem) {
+    return NextResponse.json({ message: "Item not found" }, { status: 404 });
+  }
+
+  // Returns a success response if the item was deleted.
+  return NextResponse.json({ message: "Item deleted" }, { status: 200 });
+}
