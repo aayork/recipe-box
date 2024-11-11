18fe25b60ad067133b49b05102c519ac3dd9e0d6
Ainsley Larence <ainsleylarence@MacBook-Air.local>
2024-11-10 21:15:21 -0500

    updated churros description

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ const recipeData = [
   {
     id: "1",
     title: "Churros",
-    description: "Yummy yummy in my tummy",
+    description: "Yummy yummy in my tummy!",
     image:
       "https://www.allrecipes.com/thmb/qq9s8jlKplKUDEo3Gtk15EAJpHc=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/ALR-recipe-24700-churros-VAT-hero-03-4x3-a7f6af1860934b0385f84ab9f13f2613.jpg",
   },
