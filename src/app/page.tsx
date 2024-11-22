ecb5c28de95bba0e85b47fbd70d6df5d2b11ba53
Aidan York <113198662+aayork@users.noreply.github.com>
2024-11-22 12:04:11 -0500

    rename a couple folders

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,19 +14,16 @@ export default function Home() {
   return (
     <div className="min-h-screen p-4 font-[family-name:var(--font-geist-sans)]">
       <CarouselItems />
-      {/* Centered Trending Section */}
-      <div className="flex flex-col items-center mt-4">
-        <div className="flex items-center">
-          <h2 className="font-bold text-xl mt-2">Trending</h2>
-          {signedIn && (
-            <Link href="/add-recipe">
-              <button className="ml-2 flex items-center gap-2 p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 border border-input">
-                <Plus size={16} />
-                Add New Recipe
-              </button>
-            </Link>
-          )}
-        </div>
+      <div className="flex justify-start items-center mt-4">
+        <h2 className="font-bold text-xl mt-2">Trending</h2>
+        {signedIn && (
+          <Link href="/add-recipe">
+            <button className="m-2 flex items-center gap-2 p-2 bg-blue-500 text-white rounded-lg hover:bg-blue-600 border border-input">
+              <Plus size={16} />
+              Add New Recipe
+            </button>
+          </Link>
+        )}
       </div>
       {/* Break Between Trending and Cards */}
       <div className="my-6"></div>
