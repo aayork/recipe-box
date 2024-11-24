8cec76d049dba4482292b40515b6f3d93b730ef5
Aidan York <113198662+aayork@users.noreply.github.com>
2024-11-24 16:07:29 -0500

    need user id

--- src/models/UserSchema.ts
+++ src/models/UserSchema.ts
@@ -1,17 +1,21 @@
 import mongoose, { Document, Schema } from "mongoose";
+import { v4 as uuidv4 } from "uuid"; // For generating unique UUIDs
 
 export interface IUser extends Document {
-    username: String;
-    email: String;
-    password: String;
-    userId: String;
+  userId: string;
+  username: string;
+  email: string;
+  password: string;
 }
 
-const userSchema = new Schema<IUser>({
+const userSchema = new Schema<IUser>(
+  {
+    userId: { type: String, default: () => uuidv4() }, // Use UUID as the unique ID
     username: { type: String, required: true },
     email: { type: String, required: true },
     password: { type: String, required: true },
-    userId: { type: String, unique: true, required: true },
-});
-
-export const User = mongoose.models.User ?? mongoose.model("User", userSchema);
+  },
+  {
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
+  },
+);
